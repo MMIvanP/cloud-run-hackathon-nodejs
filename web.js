0aacbb8ae54dc2e019f3eb1f3f69acc215f123e1
Ivan Poon <ivan.poon@mediamonks.com>
2022-09-09 14:40:22 +1000

    commit

--- web.js
+++ web.js
@@ -11,23 +11,23 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   console.log(req.body);
   const moves = ['F', , 'T','L', 'R'];
-  const myURL = 'https://cloud-run-hackathon-nodejs-esifpexcba-uc.a.run.app';
-  const everyoneState = req.body.arena.state;
-  const myState = req.body.arena.state['https://cloud-run-hackathon-nodejs-esifpexcba-uc.a.run.app'];
-  const myLocation = [myState.x, myState.y];
-  const myX = myState.x;
-  const myY = myState.y;
-  const myDirection = myState.direction;
-  const wasHit = myState.wasHit;
+  // const myURL = 'https://cloud-run-hackathon-nodejs-esifpexcba-uc.a.run.app';
+  // const everyoneState = req.body.arena.state;
+  // const myState = req.body.arena.state['https://cloud-run-hackathon-nodejs-esifpexcba-uc.a.run.app'];
+  // const myLocation = [myState.x, myState.y];
+  // const myX = myState.x;
+  // const myY = myState.y;
+  // const myDirection = myState.direction;
+  // const wasHit = myState.wasHit;
   
   // TODO add your implementation here to replace the random response
   try{
-    console.log('Here are all the states');
-    console.log(req.body.arena.state);
-    console.log(`WHERE AM I?`);
-    console.log(myLocation);
-    console.log(`Where am I facing`);
-    console.log(myDirection);
+    // console.log('Here are all the states');
+    // console.log(req.body.arena.state);
+    // console.log(`WHERE AM I?`);
+    // console.log(myLocation);
+    // console.log(`Where am I facing`);
+    // console.log(myDirection);
   
     res.send(moves[Math.floor(Math.random() * moves.length)]);
   }catch{}
